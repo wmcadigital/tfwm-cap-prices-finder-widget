4d0ec24b73cb5846f8a55c902ed0cddef871fe64
Dayle Salmon <daylesalmon@gmail.com>
2021-07-22 13:51:27 +0100

    Started converting repo, to get ready for first NPM release

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import habitat from 'preact-habitat';
 import App from './components/App/App';
 
+const { REACT_APP_NAME, REACT_APP_VERSION } = process.env;
+
+// eslint-disable-next-line no-console
+console.log(`Loaded ${REACT_APP_NAME} - v${REACT_APP_VERSION}`);
+
 habitat(App).render({
-  selector: '[data-widget-host="tfwm-cap-prices-finder"]',
+  selector: `[data-widget-host="${REACT_APP_NAME}"]`,
   clean: true,
 });
